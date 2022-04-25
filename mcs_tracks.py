c780bfcfeaf46411fce0a250d28948269191d1ae
Mark Muetzelfeldt <markmuetz@gmail.com>
2022-04-25 16:38:45 +0100

    Add new classes for handling pixel data, use to animate individual track.

--- mcs_tracks.py
+++ mcs_tracks.py
@@ -1,7 +1,11 @@
+import gc
+import datetime as dt
+import math
 from pathlib import Path
 
 import cartopy
 import cartopy.geodesic
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.patches import Patch
@@ -10,6 +14,15 @@ import pandas as pd
 import shapely
 import shapely.ops
 import xarray as xr
+
+
+def create_fig_ax():
+    fig, ax = plt.subplots(
+        subplot_kw=dict(projection=cartopy.crs.PlateCarree())
+    )
+    fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95)
+    ax.coastlines()
+    return fig, ax
 
 
 def round_times_to_nearest_second(dstracks):
@@ -42,6 +55,264 @@ def round_times_to_nearest_second(dstracks):
     dstracks.end_basetime.values[tmask] = vec_remove_time_incaccuracy(
         dstracks.end_basetime.values[tmask].astype(int)
     )
+
+
+def ticks_from_min_max_lon_lat(minlon, maxlon, minlat, maxlat, dlon=5, dlat=5):
+    tminlon = (minlon + dlon) - (minlon % dlon)
+    tmaxlon = maxlon - (maxlon % dlon)
+    tminlat = (minlat + dlat) - (minlat % dlat)
+    tmaxlat = maxlat - (maxlat % dlat)
+    return np.arange(tminlon, tmaxlon + dlon, dlon), np.arange(tminlat, tmaxlat + dlat, dlat)
+
+
+class PixelData:
+    @classmethod
+    def set_pixel_data_dir(cls, pixel_data_dir):
+        track_pixel_paths = sorted(pixel_data_dir.glob('*/*.nc'))
+        cls.date_path_map = {
+            dt.datetime.strptime(p.stem, 'mcstrack_%Y%m%d_%H%M'): p
+            for p in track_pixel_paths
+        }
+
+    @classmethod
+    def get_frame(cls, time):
+        return PixelFrame(time, xr.open_dataset(cls.date_path_map[time]))
+
+    @classmethod
+    def get_frames(cls, starttime, endtime):
+        time = starttime
+        paths = []
+        times = []
+        while time <= endtime:
+            if time in cls.date_path_map:
+                paths.append(cls.date_path_map[time])
+            else:
+                print(f'Warning: missing path: {path}')
+            times.append(time)
+            time += dt.timedelta(hours=1)
+        return PixelFrames(times, xr.open_mfdataset(paths))
+
+    @classmethod
+    def anim_plot_track(cls, track, method='contourf', method_kwargs=None, zoom=False):
+        times = pd.DatetimeIndex(track.base_time)
+        start = pd.Timestamp(track.dstrack.start_basetime.values).to_pydatetime()
+        end = pd.Timestamp(track.dstrack.end_basetime.values).to_pydatetime()
+
+        cloudnumbers = track.cloudnumber
+        frames = cls.get_frames(start, end)
+        extent = frames.get_min_max_lon_lat(cloudnumbers)
+        dlon = extent[1] - extent[0]
+        dlat = extent[3] - extent[2]
+        aspect = (dlon + 3) / (dlat * 3)
+        height = 9.5
+
+        fig = plt.figure('track animation')
+        fig.set_size_inches(height * aspect, height)
+        fig.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.9, hspace=0.05)
+
+        precip = frames.get_data('precipitation', cloudnumbers)
+        tb = frames.dspixel.tb.values
+        cn = frames.get_data('cloudnumber', cloudnumbers)
+
+        cn_swath = cn.sum(axis=0).astype(bool)
+        precip_swath = precip.sum(axis=0)
+
+        legend_elements = [
+            Patch(facecolor='none', edgecolor='black', label='Tb = 241K'),
+            Patch(facecolor='none', edgecolor='red', label='Cloud mask'),
+            Line2D([0], [0], color='g', label='track'),
+            Patch(facecolor='none', edgecolor='grey', label='Track cloud (circle)'),
+            Patch(facecolor='none', edgecolor='blue', label='Track PFx3 (circle)'),
+        ]
+
+        print(precip.max())
+
+        def precip_levels(pmax):
+            pmax10 = math.ceil(pmax / 10) * 10
+            return np.array([0] + list(2**np.arange(7))) / 2**6 * math.ceil(pmax10)
+
+        frame_precip_levels = precip_levels(precip.max())
+        frame_norm = mpl.colors.BoundaryNorm(boundaries=frame_precip_levels, ncolors=256)
+        swath_precip_levels = precip_levels(precip_swath.max())
+        swath_norm = mpl.colors.BoundaryNorm(boundaries=swath_precip_levels, ncolors=256)
+
+        if precip.max() > frame_precip_levels[-1]:
+            extend = 'max'
+        else:
+            extend = 'neither'
+
+        for i, (cloudnumber, time) in enumerate(zip(cloudnumbers, times)):
+            print(time)
+            fig.clf()
+            ax1 = fig.add_subplot(3, 1, 1, projection=cartopy.crs.PlateCarree())
+            ax2 = fig.add_subplot(3, 1, 2, projection=cartopy.crs.PlateCarree())
+            ax3 = fig.add_subplot(3, 1, 3)
+
+            ax1.set_title(f'Track {track.track_id} @ {time}')
+            ax1.coastlines()
+            ax2.coastlines()
+            track.plot(times=[time], ax=ax1)
+            track.plot(times=[time], ax=ax2)
+
+            im1 = ax1.contourf(frames.dspixel.lon, frames.dspixel.lat, precip[i],
+                               cmap='Blues', norm=frame_norm, levels=frame_precip_levels, extend=extend)
+            ax1.contour(frames.dspixel.lon, frames.dspixel.lat, cn[i], levels=[0.5], colors='red')
+            ax1.contour(frames.dspixel.lon, frames.dspixel.lat, tb[i], levels=[241], colors='black')
+            plt.colorbar(im1, ax=ax1, label='masked precip. (mm hr$^{-1}$)')
+
+            im2 = ax2.contourf(frames.dspixel.lon, frames.dspixel.lat, precip_swath,
+                               cmap='Blues', norm=swath_norm, levels=swath_precip_levels)
+            ax2.contour(frames.dspixel.lon, frames.dspixel.lat, cn_swath, levels=[0.5], colors='red')
+
+            cbar2 = plt.colorbar(im2, ax=ax2, label='masked precip. swath (mm)')
+            cbar2.set_ticks(swath_precip_levels)
+            cbar2.set_ticklabels([f'{v:.2f}' for v in swath_precip_levels])
+
+            ax1.set_extent(extent)
+            ax2.set_extent(extent)
+
+            lon_ticks, lat_ticks = ticks_from_min_max_lon_lat(*extent)
+
+            def fmt_lat_ticklabels(ticks):
+                labels = []
+                for t in ticks:
+                    if t < 0:
+                        labels.append(f'{-t:.0f} °S')
+                    else:
+                        labels.append(f'{t:.0f} °N')
+                return labels
+
+            def fmt_lon_ticklabels(ticks):
+                labels = []
+                for t in ticks:
+                    if t < 0:
+                        labels.append(f'{-t:.0f} °W')
+                    else:
+                        labels.append(f'{t:.0f} °E')
+                return labels
+
+            ax1.set_yticks(lat_ticks)
+            ax1.set_yticklabels(fmt_lat_ticklabels(lat_ticks))
+
+            ax2.set_yticks(lat_ticks)
+            ax2.set_yticklabels(fmt_lat_ticklabels(lat_ticks))
+
+            ax2.set_xticks(lon_ticks)
+            ax2.set_xticklabels(fmt_lon_ticklabels(lon_ticks))
+
+            ax1.legend(handles=legend_elements)
+            for name, data in [
+                ('area', track.area),
+                ('PF area', np.nanmean(track.pf_area, axis=1)),
+                ('PF rainrate', np.nanmean(track.pf_rainrate, axis=1)),
+            ]:
+                ax3.plot(data / data.max(), label=f'{name} (max={data.max():.1f})')
+            ax3.set_xlim((-0.5, track.duration - 0.5))
+            ax3.set_ylim((0, 1.6))
+            ax3.set_yticks([0, 0.5, 1])
+            ax3.axvline(x=i)
+            ax3.legend()
+
+            figpath = Path(f'figs/anim_plot_track/track_{track.track_id}'
+                           f'/track_{track.track_id}_{i:03d}.png')
+            figpath.parent.mkdir(parents=True, exist_ok=True)
+            plt.savefig(figpath)
+            # plt.pause(0.1)
+            # if input('q to quit: ') == 'q':
+            #     raise Exception('quit')
+
+    @classmethod
+    def plot_track(cls, field, track, method='contourf', method_kwargs=None, ax=None, zoom=False):
+        start = pd.Timestamp(track.dstrack.start_basetime.values).to_pydatetime()
+        end = pd.Timestamp(track.dstrack.end_basetime.values).to_pydatetime()
+        frames= cls.get_frames(start, end)
+
+        cloudnumbers = track.cloudnumber
+        frames.plot(field, method=method, method_kwargs=method_kwargs,
+                    cloudnumbers=cloudnumbers, ax=ax, zoom=zoom)
+
+
+class PixelFrames:
+    def __init__(self, time, dspixel):
+        self.time = time
+        self.dspixel = dspixel
+
+    def get_min_max_lon_lat(self, cloudnumbers):
+        assert len(self.dspixel.time) == len(cloudnumbers)
+        minlon = math.inf
+        maxlon = -math.inf
+        minlat = math.inf
+        maxlat = -math.inf
+        lon = self.dspixel.longitude.load().values
+        lat = self.dspixel.latitude.load().values
+        data_cloudnumber = self.dspixel.cloudnumber.load()
+        for i, cloudnumber in enumerate(cloudnumbers):
+            cloudmask = (self.dspixel.cloudnumber[i] == cloudnumber)
+            minlon = min(lon[i][cloudmask].min(), minlon)
+            maxlon = max(lon[i][cloudmask].max(), maxlon)
+            minlat = min(lat[i][cloudmask].min(), minlat)
+            maxlat = max(lat[i][cloudmask].max(), maxlat)
+        return (minlon, maxlon, minlat, maxlat)
+
+    def get_data(self, field, cloudnumbers):
+        data = getattr(self.dspixel, field).values
+        if len(cloudnumbers):
+            data = data.copy()
+            for i, cloudnumber in enumerate(cloudnumbers):
+                cloudmask = (self.dspixel.cloudnumber[i] == cloudnumber)
+                data[i][~cloudmask] = 0
+        return data
+
+    def plot(self, field, method='contourf', method_kwargs=None, cloudnumbers=None, ax=None, zoom=False):
+        if method not in ['contour', 'contourf']:
+            raise ValueError("method must be one of 'contour', 'contourf'")
+        if not ax:
+            fig, ax = create_fig_ax()
+            ax.set_title(f'{field} @ {self.time}')
+        if len(cloudnumbers) and zoom:
+            extent = self.get_min_max_lon_lat(cloudnumbers)
+            ax.set_extent(extent)
+        if not method_kwargs:
+            method_kwargs = {}
+
+        data = getattr(self.dspixel, field).values
+        if len(cloudnumbers):
+            data = data.copy()
+            for i, cloudnumber in enumerate(cloudnumbers):
+                cloudmask = (self.dspixel.cloudnumber[i] == cloudnumber)
+                data[i][~cloudmask] = 0
+        mean_data = data.mean(axis=0)
+        getattr(ax, method)(self.dspixel.lon, self.dspixel.lat, mean_data, **method_kwargs)
+
+
+class PixelFrame:
+    def __init__(self, time, dspixel):
+        self.time = time
+        self.dspixel = dspixel
+
+    def plot(self, field, method='contourf', method_kwargs=None, cloudnumber=None, ax=None, zoom=False):
+        if method not in ['contour', 'contourf']:
+            raise ValueError("method must be one of 'contour', 'contourf'")
+        if not ax:
+            fig, ax = create_fig_ax()
+            ax.set_title(f'{field} @ {self.time}')
+        if cloudnumber:
+            cloudmask = (self.dspixel.cloudnumber == cloudnumber)[0]
+            print(cloudmask.sum())
+            if zoom:
+                minlon = self.dspixel.longitude.values[cloudmask].min()
+                maxlon = self.dspixel.longitude.values[cloudmask].max()
+                minlat = self.dspixel.latitude.values[cloudmask].min()
+                maxlat = self.dspixel.latitude.values[cloudmask].max()
+                ax.set_extent([minlon, maxlon, minlat, maxlat])
+        if not method_kwargs:
+            method_kwargs = {}
+
+        data = getattr(self.dspixel, field)[0].values
+        if cloudnumber:
+            data = data.copy()
+            data[~cloudmask] = 0
+        getattr(ax, method)(self.dspixel.lon, self.dspixel.lat, data, **method_kwargs)
 
 
 class McsTracks:
@@ -77,8 +348,8 @@ class McsTracks:
             ax.coastlines()
             ax.set_title(f'{self}')
             if times != 'all' and len(times) == 1:
-                dspixel = xr.open_dataset(self.date_path_map[times[0]])
-                ax.contourf(dspixel.lon, dspixel.lat, dspixel.cloudnumber[0])
+                frame = PixelData.get_frame(times[0])
+                frame.plot('cloudnumber', ax=ax)
 
         for i, track_id in enumerate(self.dstracks.tracks):
             track = self.get_track(track_id.values.item())
@@ -127,12 +398,10 @@ class McsTrack:
             ax.coastlines()
             ax.set_title(f'{self}')
             if times != 'all' and len(times) == 1:
-                dspixel = xr.open_dataset(self.date_path_map[times[0]])
                 time_index = pd.Timestamp(times[0]).to_numpy()
                 cloudnumber = self.cloudnumber[self.base_time == time_index]
-                ax.contour(
-                    dspixel.lon, dspixel.lat, dspixel.cloudnumber[0] == cloudnumber
-                )
+                frame = PixelData.get_frame(times[0])
+                frame.plot('precipitation', 'contour', cloudnumber=cloudnumber, ax=ax, zoom=True)
 
         ax.plot(self.meanlon, self.meanlat, 'g-')
 
@@ -158,6 +427,7 @@ class McsTrack:
             for i in time_indices:
                 lon = self.meanlon[i]
                 lat = self.meanlat[i]
+                ax.plot(lon, lat, 'go')
                 radius = np.sqrt(self.ccs_area[i].item() / np.pi) * 1e3
                 if lon < -170:
                     continue
@@ -214,8 +484,10 @@ class McsTrack:
 
 
 if __name__ == '__main__':
+    # plt.ion()
     stats_dir = Path('/home/markmuetz/Datasets/MCS_PRIME/MCS_database/MCS_Global/stats')
-    stats_year_path = stats_dir / 'mcs_tracks_final_extc_20000601.0000_20010101.0000.nc'
+    # 2019.
+    stats_year_path = stats_dir / 'mcs_tracks_final_extc_20190101.0000_20200101.0000.nc'
     try:
         dstracks
     except NameError:
@@ -227,7 +499,23 @@ if __name__ == '__main__':
     )
 
     tracks = McsTracks(dstracks, pixel_data_dir)
-    time = dt.datetime(2000, 6, 20, 0, 30)
-    tracks.tracks_at_time(time).plot(times=[time])
-    tracks.get_track(1946).plot(times=[dt.datetime(2000, 6, 20, 00, 30)])
-    plt.show()
+    PixelData.set_pixel_data_dir(pixel_data_dir)
+    time = dt.datetime(2019, 6, 21, 6, 30)
+    selected_tracks = tracks.tracks_at_time(time)
+    # track = tracks.get_track(15378)
+
+    track = tracks.get_track(15477)
+    PixelData.anim_plot_track(track, zoom=True)
+
+    # gc.collect()
+
+    # track = tracks.get_track(15463)
+    # PixelData.anim_plot_track(track, zoom=True)
+
+    # for track_id in selected_tracks.dstracks.tracks.values:
+    #     print('='*3, track_id, '='*3)
+    #     track = tracks.get_track(track_id)
+    #     print(track)
+    #     PixelData.anim_plot_track(track, zoom=True)
+    #     gc.collect()
+    # plt.show()

36c10e8210de1f34f773a2c0b5a50dcafa90aa86
Mark Muetzelfeldt <markmuetz@gmail.com>
2022-08-29 17:08:46 +0100

    Work on linking MCS to shear conditions. Shear defined between levels.

--- ctrl/remakefiles/track_era5_env_precursor.py
+++ ctrl/remakefiles/track_era5_env_precursor.py
@@ -141,20 +141,147 @@ class TrackERA5EnvPrecursor(TaskRule):
         print(end - start)
 
 
+class TrackERA5EnvPrecursorShear(TaskRule):
+    rule_inputs = {}
+    rule_outputs = {'daily_track_era5_data': (PATHS['outdir'] / 'track_era5_env_precursor' /
+                                              '{year}' /
+                                              'daily_track_era5_data_shear_{year}_batch-{batch:02d}.nc')}
+
+    var_matrix = {('year', 'batch'): batch_info}
+
+    def rule_run(self):
+        # Load the tracks data.
+        stats_year_path = list(PATHS['statsdir'].glob(f'mcs_tracks_final_extc_{self.year}*.0000.nc'))[0]
+        tracks = McsTracks.open(stats_year_path, PATHS["pixeldir"])
+        start_track = self.batch * NTRACKS_PER_BATCH
+        end_track = (self.batch + 1) * NTRACKS_PER_BATCH
+        tracks = McsTracks(tracks.dstracks.isel(tracks=slice(start_track, end_track)), PATHS['pixeldir'])
+
+        start_time = pd.Timestamp(tracks.dstracks.start_basetime.values.min()).to_pydatetime()
+        end_time = pd.Timestamp(tracks.dstracks.end_basetime.values.max()).to_pydatetime()
+        print(start_time, end_time)
+
+        start = default_timer()
+        # Force load of some data.
+        tracks.dstracks.base_time.load()
+        tracks.dstracks.meanlon.load()
+        tracks.dstracks.meanlat.load()
+
+        variables = ['u', 'v']
+        # Pick out 4 model levels to work out low/mid/deep shear.
+        # Levels defined here:
+        # https://confluence.ecmwf.int/display/UDOC/L137+model+level+definitions
+        # pressure/height shown for surface=1013.25hPa.
+        levels = [
+            133,  # 1000hPa/107m
+            111,  # 804hPa/1911m
+            96,  # 508hPa/5469m
+            74,  # 197hPa/11890m
+        ]
+
+        # Create an output dataset that is similar to the tracks dataset for the vars I am getting.
+        var_data = {}
+        for var in variables:
+            data = np.full([
+                len(tracks.dstracks.tracks),
+                len(tracks.dstracks.times),
+                len(levels)
+            ], np.nan)
+            var_data[var] = (('tracks', 'times', 'level'), data)
+        dsout = xr.Dataset(
+            coords=dict(tracks=tracks.dstracks.tracks, times=tracks.dstracks.times, level=levels),
+            data_vars=var_data,
+        )
+
+        num_filtered_points = 0
+
+        track_time = start_time
+        while track_time <= end_time:
+            print(track_time)
+            # track data every hour on the half hour.
+            # ERA5 data every hour on the hour.
+            e5time1 = track_time - dt.timedelta(minutes=30)
+            e5time2 = e5time1 + dt.timedelta(hours=1)
+
+            # Cannot interp track data - get ERA5 before and after and interp
+            # using e.g. ...mean(dim=time).
+            paths = [(PATHS['era5dir'] / f'data/oper/an_ml/{t.year}/{t.month:02d}/{t.day:02d}' /
+                      (f'ecmwf-era5_oper_an_ml_{t.year}{t.month:02d}{t.day:02d}'
+                       f'{t.hour:02d}00.{var}.nc'))
+                     for var in variables
+                     for t in [e5time1, e5time2]]
+            # Lat limited to that of tracks, and midpoint time value.
+            e5 = (xr.open_mfdataset(paths).sel(latitude=slice(60, -60), level=levels)
+                  .mean(dim='time').load())
+
+            track_point_mask = tracks.dstracks.base_time == pd.Timestamp(track_time)
+            track_point_lon = tracks.dstracks.meanlon.values[track_point_mask]
+            track_point_lat = tracks.dstracks.meanlat.values[track_point_mask]
+
+            # Convert -180-180 -> 0-360.
+            track_point_lon = track_point_lon % 360
+
+            # This is the same way you select values along a transect.
+            # But here I just want at unconnected points.
+            lon = xr.DataArray(track_point_lon, dims='track_point')
+            lat = xr.DataArray(track_point_lat, dims='track_point')
+
+            if len(lon):
+                var_data = {}
+                for var in variables:
+                    # TODO: Could solve this with cartopy.util.add_cyclic_point.
+                    data = e5[var].interp(longitude=lon, latitude=lat).values
+                    # if lon > 359.75, cannot interp and value is nan.
+                    # record number of times this happens.
+                    mask = np.isnan(data)
+                    num_filtered_points += mask.sum()
+                    # Note, mask works on dsout[var] because it has the same shape as dstracks.
+                    # data comes from ERA5, and has coords (level, npoints),
+                    # dsout has coords which are the opposite way round, because mask selects npoint's
+                    # worth of data and the final coord is levels.
+                    # hence use of data.T.
+                    dsout[var].values[track_point_mask] = data.T
+
+            e5.close()
+            track_time += dt.timedelta(hours=1)
+
+        dsout.attrs = dict(
+            num_filtered_points=num_filtered_points,
+        )
+        end = default_timer()
+        comp = dict(zlib=True, complevel=4)
+        encoding = {var: comp for var in dsout.data_vars}
+        dsout.to_netcdf(self.outputs['daily_track_era5_data'], encoding=encoding)
+        print(end - start)
+
+
 class CombineTrackERA5EnvPrecursor(TaskRule):
     @staticmethod
-    def rule_inputs(year):
-        inputs = {f'batch-{b:02d}': fmtp(TrackERA5EnvPrecursor.rule_outputs['daily_track_era5_data'],
-                                         year=year,
-                                         batch=b)
-                  for b in range(nbatch_per_year[year])}
+    def rule_inputs(year, datatype):
+        if datatype == '2D':
+            inputs = {f'batch-{b:02d}': fmtp(TrackERA5EnvPrecursor.rule_outputs['daily_track_era5_data'],
+                                             year=year,
+                                             batch=b)
+                      for b in range(nbatch_per_year[year])}
+        elif datatype == 'shear':
+            inputs = {f'shear-batch-{b:02d}': fmtp(TrackERA5EnvPrecursorShear.rule_outputs['daily_track_era5_data'],
+                                                   year=year,
+                                                   batch=b)
+                      for b in range(nbatch_per_year[year])}
         return inputs
 
-    rule_outputs = {'track_era5_data': (PATHS['outdir'] / 'track_era5_env_precursor' /
-                                        '{year}' /
-                                        'daily_track_era5_data_{year}.nc')}
+    def rule_outputs(year, datatype):
+        if datatype == '2D':
+            outputs = {'track_era5_data': (PATHS['outdir'] / 'track_era5_env_precursor' /
+                                           f'{year}' /
+                                           f'daily_track_era5_data_{year}.nc')}
+        elif datatype == 'shear':
+            outputs = {'track_era5_data': (PATHS['outdir'] / 'track_era5_env_precursor' /
+                                           f'{year}' /
+                                           f'daily_track_era5_data_shear_{year}.nc')}
+        return outputs
 
-    var_matrix = {'year': years}
+    var_matrix = {'year': years, 'datatype': ['2D', 'shear']}
 
     def rule_run(self):
         input_paths = self.inputs.values()
